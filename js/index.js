fd13bdc440b00352792bd55ea1f9cf1b142b2920
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2024-12-19 17:54:58 +0200

    home work

--- js/index.js
+++ js/index.js
@@ -15,27 +15,63 @@ function logItems(array) {
   // console.log(calculateEngravingPrice('Hello world', 10));
   // console.log(calculateEngravingPrice('JavaScript is fun', 15));
 
-// 3
-function findLongestWord(string) {
-  const words = string.split(' ');
-  let longestWord = '';
-  for (const word of words) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  }
-  return longestWord;
-}
+// // 3
+// function findLongestWord(string) {
+//   const words = string.split(' ');
+//   let longestWord = '';
+//   for (const word of words) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+//   return longestWord;
+// }
 
-console.log(findLongestWord("I sigma now no computer"));
+// console.log(findLongestWord("I sigma now no computer"));
 
-// 4
-function formatString(string) {
-  if (string.length > 40) {
-    return string.slice(0, 40) + '...';
-  }
-  return string;
-}
-console.log(formatString("hsdgfdgsgdfgsdgfygsyfgsdgyfsgfgsfgsyfgsyfysgfgsf ge ge geeeeeeeeergcgsyugsygs")); 
+// // 4
+// function formatString(string) {
+//   if (string.length > 40) {
+//     return string.slice(0, 40) + '...';
+//   }
+//   return string;
+// }
+// console.log(formatString("Lorem vvbdbd dfgdgdf fddfdfgfgdfddfgd ddgdfd")); 
 
+// 5
 
+// function checkForSpam(message) {
+//   const lowerCaseMessage = message.toLowerCase();
+//   return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")
+// }
+
+// console.log(checkForSpam("total sPaM more"))
+
+// 6
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// while (true) {
+//   input = prompt("введіть число")
+//   if (input === null)
+//     break
+
+// const number = Number(input)
+
+// if (isNaN(number)) {
+//   alert("Було введено не число, попробуйте ще раз")
+//   continue
+// }
+
+// numbers.push(number)
+
+// for (const num of numbers){
+//   total += num
+// }
+// if (numbers.length > 0){
+//   console.log(`загальна сума ${total}`)
+// } else{
+//   console.log(`сума дорівняє 0`)
+// }
+// }
